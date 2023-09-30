39734b036c2e60f6cd37f4710f4b95eb692b94c7
Anirudha Havaldar <anirudhahavaldar@gmail.com>
2023-09-30 08:49:33 +0530

    Day - 03 Random Password Generator completed

--- Day - 03 Random Password Generator/script.js
+++ Day - 03 Random Password Generator/script.js
@@ -1,10 +1,29 @@
 import { characters } from './characters.js';
-console.log(characters.length);
-console.log(Math.floor(Math.random() * characters.length));
+const generateBtn = document.getElementById('generate-btn');
+const password = document.getElementById('password');
+const copy = document.getElementById('copy');
 
-while (Math.floor(Math.random() * characters.length) + 1 === 94) {
-  console.log('yes');
-}
 function randomCharacter() {
-  return Math.floor(Math.random() * characters.length);
+  return characters[Math.floor(Math.random() * characters.length)];
 }
+
+generateBtn.addEventListener('click', () => {
+  let password = '';
+  for (let i = 1; i < 12; i++) {
+    password += randomCharacter();
+  }
+  render(password);
+});
+
+function render(passedPassword) {
+  password.innerHTML = `${passedPassword}`;
+}
+
+copy.addEventListener('click', () => {
+  if (password.textContent === '') {
+    alert('Please generate a password to copy');
+  } else {
+    navigator.clipboard.writeText(password.textContent);
+    copy.innerText = 'Copied';
+  }
+});
